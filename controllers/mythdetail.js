32f99e7fe28e48f9831d9923ee417ebb45e26f56
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-10-20 14:52:34 -0400

    added referencing and pushing reviews to myth, rendered reviews onto mythdetail show page

--- controllers/mythdetail.js
+++ controllers/mythdetail.js
@@ -3,6 +3,12 @@ import {MythReview} from './../models/mythreview.js';
 
 function show(req, res){
     Myth.findById(req.params.id)
+    .populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    })
     .then(myth => {
         res.render("mythdetail/show", {
             title: `Folktale Info`,
@@ -12,14 +18,25 @@ function show(req, res){
 }
 
 function create(req, res){
-    req.body.author = req.user.profile._id
+
     req.body.myth = req.params.id
+    req.body.author = req.user.profile._id
     
     console.log(req.body)
     // 
     MythReview.create(req.body)
+    // review is the specified review created at this time
     .then(review => {
-        res.redirect(`/mythdetail/${req.params.id}`)
+        Myth.findById(req.params.id)
+        .then(myth => {
+//review(s) on the mythmodel   //review that was just created
+            myth.reviews.push(review._id)
+            myth.save()
+            .then(() => {
+                res.redirect(`/mythdetail/${req.params.id}`)    
+            })
+        })
+        
     })
 }
 
